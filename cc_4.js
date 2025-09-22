ef48bc917ad59a70d711accbb4424a721a2ac294
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-09-21 22:03:21 -0400

    Array of products

---
+++ cc_4.js
@@ -0,0 +1,14 @@
+// Coding Challenge 4: Retail Discount Engine
+
+// Array of products
+let products = [
+    { name: "Headphones", category: "Electronics", price: 45.00, inventory: 30 },
+    { name: "Lamp", category: "household", price: 25.00, inventory: 15 },
+    { name: "Apples", category: "Grocery", price: 3.00, inventory: 100 },
+    { name: "Jeans", category: "Apparel", price: 60.00, inventory: 20 },
+    { name: "Lipstick", category: "Beauty", price: 20.00, inventory: 50 }
+]
+
+
+
+
