e0d66cbdc4150875013d53f4dd714175525182d0
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-09-21 22:24:44 -0400

    Customer Data

--- cc_4.js
+++ cc_4.js
@@ -2,11 +2,11 @@
 
 // Array of products
 let products = [
-    { name: "Headphones", category: "electronics", price: 45.00, inventory: 30 },
-    { name: "Lamp", category: "household", price: 25.00, inventory: 15 },
-    { name: "Apples", category: "groceries", price: 3.00, inventory: 100 },
-    { name: "Jeans", category: "apparel", price: 60.00, inventory: 20 },
-    { name: "Lipstick", category: "beauty", price: 20.00, inventory: 50 }
+    { sku: "001", name: "Headphones", category: "electronics", price: 45.00, inventory: 30 },
+    { sku: "002", name: "Lamp", category: "household", price: 25.00, inventory: 15 },
+    { sku: "003", name: "Apples", category: "groceries", price: 3.00, inventory: 100 },
+    { sku: "004", name: "Jeans", category: "apparel", price: 60.00, inventory: 20 },
+    { sku: "005", name: "Lipstick", category: "beauty", price: 20.00, inventory: 50 }
 ];
 
 // Category Discounts
@@ -30,3 +30,15 @@ for (let product of products) {
 }
 
 console.log(products);
+
+// Customer Data 
+const customers = [
+    { name: "Alice", customerType: "student", couponCode: "STUDENT5", taxRate: 0.085,
+        cart: [ { sku: "005", quantity: 1 }, { sku: "003", quantity: 5 } ] },
+    { name: "Bob", customerType: "senior", couponCode: "SENIOR07", taxRate: 0.085,
+        cart: [ { sku: "002", quantity: 2 }, { sku: "004", quantity: 1 } ] },
+    { name: "Charlie", customerType: "regular", couponCode: null, taxRate: 0.085,
+        cart: [ { sku: "001", quantity: 1 }, { sku: "002", quantity: 1 }, { sku: "003", quantity: 2 } ] }
+];
+    
+    ]
